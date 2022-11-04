cca2150ca29c3108de6e30bd5f2a9eedbaa6db45
Minsun91 <minsunkim167@gmail.com>
2022-11-04 21:43:36 +0100

    rm cache

--- dist/src/layers/controllers/post.controller.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var PostService = require("../services/post.service");
-var joi = require("joi");
-var post = require("../../../sequelize/models/post");
--- dist/src/routers/auth.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const authRouter = (0, express_1.Router)();
-exports.authRouter = authRouter;
-authRouter.post("/signup", auth_controller_1.signUp);
-authRouter.post("/signin", auth_controller_1.signIn);
